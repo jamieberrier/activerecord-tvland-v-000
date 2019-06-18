daf83d34c27f4c93e90e0d4271951aba72402e7d
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 18:38:56 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
 
   # lists all of the characters that actor has.
   def list_roles
-    binding.pry
+    #binding.pry
     "#{self.characters.name} - #{self.shows.name}"
   end
 end
