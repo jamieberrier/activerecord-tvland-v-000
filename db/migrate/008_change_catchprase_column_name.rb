b2880254bf28832643889d70dde1da8fb7e66c53
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 18:53:14 +0000

    Automatically backed up by Learn

--- db/migrate/008_change_catchprase_column_name.rb
+++ db/migrate/008_change_catchprase_column_name.rb
@@ -1,5 +1,5 @@
 class ChangeCatchpraseColumnName < ActiveRecord::Migration[4.2]
   def change
-    delete_column :characters, :catchprase, :string
+    rename_column :characters, :catchprase, :string
   end
 end
