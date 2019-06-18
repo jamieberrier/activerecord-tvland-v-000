7fbced14ee521decce8a6332aeb55be021725805
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 18:09:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,6 @@
 class Actor < ActiveRecord::Base
 #  has_many :songs
 #  has_many :genres, through: :songs
-  has_many :characters, :shows
+  has_many :characters
+  has_many :shows, through :characters
 end
