ce47b95091f2c93a6e9ce396ad9cbf5a4869ed43
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 18:23:24 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchprase_to_character.rb
+++ db/migrate/006_add_catchprase_to_character.rb
@@ -1,5 +1,5 @@
 class AddCatchpraseToCharacter < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :network_id, :integer
+    add_column :characters, :catchprase, :integer
   end
 end
