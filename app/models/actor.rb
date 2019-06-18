697827aea5e55f2420ef5f78a8ecbc51cba5ea5b
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 18:11:25 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,9 +1,9 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :shows, through :characters
+  has_many :shows, through: :characters
 
   # returns the first and last name of an actor.
   def full_name
-    
+
   end
 end
